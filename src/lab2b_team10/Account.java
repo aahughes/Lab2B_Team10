4c9552b3bfb27b0c22d59ab0143fd03a05886c98
antonizoon <sagnessagiel@gmail.com>
2016-02-01 15:26:11 -0500

    created parent Account class

---
+++ src/lab2b_team10/Account.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2b_team10;
+import java.util.Random;
+
+/**
+ *
+ * @author antonizoon
+ */
+public class Account {
+    // class variables
+    private double balance;
+    
+    // generate random account number
+    private int accountNum;
+    final int max = 0;
+    final int min = 10000;
+    
+    public Account() {
+        Random rand = new Random();
+        this.accountNum = rand.nextInt((max - min) + 1) + min;
+        this.balance = 0.0;     // start at $0.0
+    }
+    
+    public Account(double balance) {
+        Random rand = new Random();
+        this.accountNum = rand.nextInt((max - min) + 1) + min;
+        this.balance = balance;
+    }
+    
+    // accessor for balance
+    public double getBalance() {
+        return this.balance;
+    }
+    
+    // mutator for balance
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
