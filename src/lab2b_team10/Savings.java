a150b464e7e9e44bfc53588d9bbd9f7b35587cd9
antonizoon <sagnessagiel@gmail.com>
2016-02-01 15:42:28 -0500

    Created Checking Classes

---
+++ src/lab2b_team10/Savings.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2b_team10;
+
+/**
+ *
+ * @author antonizoon
+ */
+public class Savings extends Account {
+    private double interestRate;
+    
+    public Savings() {
+        super();
+    }
+    
+    public Savings(double balance) {
+        super(balance);
+    }
+    
+    // set interest rate as well
+    public Savings(double balance, double interestRate) {
+        super(balance);
+        this.interestRate = interestRate;
+    }
+    
+    // accessor for interest rate
+    public double getInterestRate() {
+        return this.interestRate;
+    }
+    
+    // mutator for interest rate
+    public void setInterestRate() {
+        this.interestRate = interestRate;
+    }
+}
