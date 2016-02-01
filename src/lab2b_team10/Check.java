12df4d539bb2776c611762416cee8da8ddd4f0d9
ELCHOCO <ELCHOCO@client-104-39-93-170.mobility.psu.edu>
2016-02-01 15:38:01 -0500

    added classes with their descriptions

---
+++ src/lab2b_team10/Check.java
@@ -0,0 +1,15 @@
+package lab2b_team10;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ELCHOCO
+ */
+public class Check {
+    
+}
