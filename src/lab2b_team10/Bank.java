4ccab6dd629b1f6a58d27f7b484474e5692b9825
Allison <Allison@104.39.128.77>
2016-02-02 19:30:54 -0500

    started method to create new accounts

--- src/lab2b_team10/Bank.java
+++ src/lab2b_team10/Bank.java
@@ -5,20 +5,49 @@
  */
 package lab2b_team10;
 
+import java.util.Scanner;
+
 /**
  *
  * @author antonizoon, aahughes
  */
 
-// hi
-// Ed 
 public class Bank {
-
+    Scanner scan = new Scanner(System.in);
+    double currentInterestRate = 0.75;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
     }
     
+    
+    public Account createAccount(){
+        boolean accountCreated = false;
+        double newBalance = 0;
+        Account newAccount;
+        while (!accountCreated){
+            System.out.println("Please specify account type.");
+            String type = scan.nextLine();
+            if (type.equals("Checking")){
+                System.out.println("Please enter initial balance.");
+                newBalance = scan.nextDouble();
+                newAccount = new Checking(newBalance);
+                accountCreated = true;
+            }
+            else if (type.equals("Savings"))  {
+                System.out.println("Please enter initial balance.");
+                newBalance = scan.nextDouble();
+                newAccount = new Savings(newBalance, currentInterestRate);
+                accountCreated = true;
+            }  
+            else{
+                System.out.println("Invalid account type. Could not create account.");
+            }
+        }
+        return newAccount;
+    }
 }
